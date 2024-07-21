b9819273a8da49f4020618c9bf1af5d7f1a9fb2a
Patthethicc <167599442+Patthethicc@users.noreply.github.com>
2024-07-21 18:35:58 +0800

    Added the inheritance of a Room

---
+++ Deluxe.java
@@ -0,0 +1,19 @@
+public class Deluxe extends Room{
+
+    private double priceDeluxe;
+
+    public Deluxe(String name, double price) {
+        super(name, price);
+        this.priceDeluxe = price * 1.2;
+    }
+
+    @Override
+    public void setPrice(double price){
+        this.priceDeluxe = price * 1.2;
+    }
+
+    @Override
+    public double getPrice(){
+        return this.priceDeluxe;
+    }
+}
---
+++ Executive.java
@@ -0,0 +1,19 @@
+public class Executive extends Room{
+
+    private double priceExecutive;
+
+    public Executive(String name, double price) {
+        super(name, price);
+        this.priceExecutive = price * 1.35;
+    }
+
+    @Override
+    public void setPrice(double price){
+        this.priceExecutive = price * 1.35;
+    }
+
+    @Override
+    public double getPrice(){
+        return this.priceExecutive;
+    }
+}
